8399fc4d5aff47fb6c01974424a2f918c3ba37b2
Ray Garcia <r.keoni@gmail.com>
2012-11-28 10:50:04 -0600

    blank template for artist, tbd content

---
+++ DplKit/python/dplkit/simple/artist.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+dplkit.simple.artist
+~~~~~~~~~~~~~~~~~~~~
+
+Matplotlib examples from HSRL
+
+:copyright: 2012 by University of Wisconsin Regents, see AUTHORS for more details
+:license: GPLv3, see LICENSE for more details
+"""
+
+import os, sys
+import logging
+from ..role.blender import aBlender
+
+LOG = logging.getLogger(__name__)
+
+
+
+class MplMultiLinePlot(aArtist):
+    """
+    """
+    def __init__(self, source, **kwargs):
+        super(Merge, self).__init__()
+        self._config = kwargs
+
+    def render(self, *args, **kwargs):
+
+
+
+
+
+
+
+def test():
+    """ """
+    pass
+
+
+if __name__=='__main__':
+    test()
