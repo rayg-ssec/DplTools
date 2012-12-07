a183a2ddec302264b654110e2963b17ae1166a13
Ray Garcia <r.keoni@gmail.com>
2012-12-07 16:23:08 -0600

    Working on time interpolated linear / spline fit module for merge filter.

--- DplKit/python/dplkit/simple/resampler.py
+++ DplKit/python/dplkit/simple/resampler.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
-package.module
-~~~~~~~~~~~~~~
+dplkit.simple.resampler
+~~~~~~~~~~~~~~~~~~~~~~~
 
 
-A description which can be long and explain the complete
-functionality of this module even with indented code examples.
-Class/Function however should not be documented here.
+Useful examples of resampling filters
 
 
 :copyright: 2012 by University of Wisconsin Regents, see AUTHORS for more details
@@ -23,31 +22,129 @@ __docformat__ = 'reStructuredText'
 
 import os, sys
 import logging, unittest
+import bisect
+from collections import defaultdict, namedtuple
+
+from ..role.resampler import aResampler # abstract base class for resampler role
+from ..frame.struct import as_struct    # allow both dictionary-style and struct-style frames
+from ..util.interp import TimeSeriesPolyInterp
 
 
 LOG = logging.getLogger(__name__)
 
-class TimeInterpolatedMerge(dplkit.role.resampler):
-    """
-    take the time frame information from the primary stream, 
-    and time-interpolate selecteed channels from the other streams into a composite stream
 
-    This will deliver all frames from the primary. Period. No excuses.
+
+
+
+
+class TimeInterpolatedMerge(aResampler):
+    """
+    Using the time frame information from the primary stream, 
+    time-interpolate channels from the other streams into a composite stream.
+
+    This will yield all frames from the primary.
     In the case that one or more of the secondaries cannot provide data, their channels are None.
     """
+    provides = None 
+    requires = None
+
+    _schedule = None  # schedule of what data comes from where, [(channel, source), ...]
+    _pool = None # current frames we're using for each source, {source : recent-frame-list, ...}
+    _pool_depth = 2  # for now, only allow pools to be 2-points long; later, we allow splining or other fits
+
+    @staticmethod
+    def _generate_meta_from_sequence(channel_seq, *sources):
+        schedule = list(channel_seq)
+        meta = dict( (chn,source.meta[chn]) for (chn,source) in schedule )
+        return meta, schedule
+
+    @staticmethod
+    def _generate_meta_from_set(channels, *sources):
+        schedule = []
+        meta = {}
+        for source in sources:
+            assert(hasattr(source, 'meta'))
+            for chn,nfo in source.meta.items():
+                if chn in meta:
+                    continue
+                if (channels is not None) and (chn not in channels):
+                    continue
+                meta[chn] = nfo
+                schedule.append((chn, source))
+        return meta, schedule
+
+    def _meta_sequence(*sources):
+        for source in sources:
+            assert(hasattr(source,'meta'))
+            for chn in source.meta.keys():
+                yield chn,source
+
+    @staticmethod
+    def _generate_meta(channels, *sources):
+        """
+        return metadata dictionary and transfer schedule using each of the upstream sources
+        """
+        if channels is not None:
+            if hasattr(channels, 'keys'):
+                return TimeInterpolatedMerge._generate_meta_from_sequence(channels.items(), *sources)
+            elif hasattr(channels, '__contains__'): 
+                return TimeInterpolatedMerge._generate_meta_from_set(channels, *sources)
+            elif iterable(channels):
+                return TimeInterpolatedMerge._generate_meta_from_sequence(channels, *sources)                
+            else:
+                raise ValueError('channels must be a mappable or set')
+        else:
+            return TimeInterpolatedMerge._generate_meta_from_sequence(TimeInterpolatedMerge._meta_sequence(*sources))
+
+
+
+
     def __init__(self, primary, secondary_list, channels=None):
         """
-        primary: the framestream that we get time information from
-        secondary_list : a collection of secondary framestreams which get merged in order
-        channels: optional set() of channel names which get merged from the secondaries
+        Initialize a time-interpolated merge filter with one primary and one or more secondary sources.
+        Overlapping channels are selected with priority on earlier (primary, then secondaries in order).
+        Metaframe for self is updated to merge primary and secondary source metadata.
+
+        :param primary: the framestream that we get time information from
+        :param secondary_list : a collection of secondary framestreams which get merged in order
+        :param channels: optional set() of channel names which get merged from the secondaries. 
+                         If this is a dictionary, the key is the channel name and value is which source to use.
         """
-        # examine .meta from primary and each of the secondaries to get list of channels
+        # examine .meta from primary and each of the secondaries to get list of channels and our .provides dict
         # compare against channels-of-interest set if provided to create a table
         # merge meta from primary with meta from secondaries, masked by channels-of-interest
         # set .meta for self
-        pass
+        self.provides, self._schedule = TimeInterpolatedMerge._generate_meta(channels, primary, *secondary_list)
+
+        LOG.debug('transfer schedule: %s' % repr(self.schedule))
+        LOG.debug('what we provide: %s' % repr(list(self.provides.keys())))
+
+        self._pool = defaultdict(list)
+
+    def _frame_intersecting(self, spool, when):
+        "return frame from source-pool intersecting a time, or None"
+
+
+    def _fill_pool_for_source(self, source, when):
+        "for a given source, fill its pool until we can cover a timeframe's midpoint"
+
+
+
+    def _drain_pool(self, ending_at):
+        "remove everything from pool older than ending_at"
+        for _,spool in self._pool.items():
+            del spool[:-self._pool_depth]
+
+
+
+
 
     def resample(self, *args, **kwargs):
+        """
+        Yield a framestream, one for each timeframe in the primary source.        
+        Interpolate the channels provided by secondary sources.
+        Channels are returned as None if the identified source of that channel was unavailable.
+        """
         # for each frame in the primary
         #  see if we have an applicable frame from each of the secondaries
         #  call secondary.next() if we need another frame in order to interpolate
@@ -58,6 +155,11 @@ class TimeInterpolatedMerge(dplkit.role.resampler):
         #  if channels is None, use all channels from the secondary
         #  create a new output frame which merges the primary data with the interpolated secondary data
         #  yield that frame
+
+        # for each frame in primary, obtain time window
+        # for each channel,source in schedule
+        #   check source 
+
         pass 
 
 
---
+++ DplKit/python/dplkit/util/interp.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+dplkit.util.interp
+~~~~~~~~~~~~~~~~~~
+
+Interpolation tools
+
+
+:copyright: 2012 by University of Wisconsin Regents, see AUTHORS for more details
+:license: GPLv3, see LICENSE for more details
+"""
+
+
+__author__ = 'R.K.Garcia <rayg@ssec.wisc.edu>'
+__revision__ = '$Id:$'
+__docformat__ = 'reStructuredText'
+
+
+import os, sys
+import logging, unittest
+import bisect
+import numpy as np
+from collections import defaultdict, namedtuple
+from datetime import datetime, timedelta
+from operator import itemgetter
+
+
+LOG = logging.getLogger(__name__)
+
+pool_data = namedtuple('pool_data', 'when data')
+pool_poly = namedtuple('pool_poly', 'start end coeffs')
+
+
+
+
+class TimeSeriesPolyInterp(object):
+    """
+    Piecewise interpolation for order 1 and up for numpy array data on a datetime sequence
+    Given a time-ordered series of (datetime, numpy-array) pairs
+    maintain a pool of recent values to interpolate between the mid-times
+
+    inputs must consistently have the same shape and type
+    times must be monotonic    
+    """
+    _order = None
+    _max_pool_len = None
+    _times = None
+    _pool = None   # list of pool_data tuples, at least order+1 long to be useful
+    _polys = None  # dict of (start,end) : pool_poly tuples
+    _shape = None  # uniform shape of the output
+
+    def __init__(self, order = 1, max_pool_len = 16):
+        self._order = order
+        self._max_pool_len = max_pool_len
+        self._times = []
+        self._pool = []  # matches _times
+        self._polys = {}
+
+    def _drain(self, no_older_than_this=None):
+        "remove polygons from poly-table that no longer are relevant"
+        if len(self._pool) > self._max_pool_len:
+            del self._pool[:-self._max_pool_len]
+            del self._times[:-self._max_pool_len]
+        if (no_older_than_this is not None) and (len(self._pool)>0):
+            n = 0
+            while self._times[0] > when:
+                n += 1
+            del self._times[:n]
+            del self._pool[:n]
+        if len(self._polys) > 2 * len(self._pool):
+            times = set(x.when for x in self._pool)
+            self._polys = dict((k,v) for (k,v) in self._pool.items() if (v.start in times) or (v.end in times))
+
+    def append(self, time_data_tuple):
+        when, data = time_data_tuple
+        data = np.array(data)
+        assert(len(self._pool)==0 or when > self._pool[-1].when)
+        if self._shape is None:
+            self._shape = data.shape
+        self._pool.append(pool_data(when, data))
+        self._times.append(when)  # used for bisect search
+
+    def __iadd__(self, seq):
+        seq = tuple(seq)
+        self._pool += list(pool_data(when,np.array(data)) for (when,data) in seq)
+        self._times += list(when for (when,data) in seq)
+        # FIXME: check that time is ever-increasing i.e. monotonic
+        if self._shape is None and self._pool:
+            self._shape = self._pool[0].data.shape
+        return self
+
+    @property
+    def span(self):
+        if len(self._pool) <= self._order: return None, None
+        return self._pool[self._order - 1].when, self._pool[-1].when
+
+    def __contains__(self, when):
+        if (len(self._pool) < (self._order+1)):
+            return False
+        start, end = self.span
+        # for order > 1, we only allow interpolation between last two segments of a given grouping, to enforce ~continuity
+        if (when < start) or (when >= end):
+            return False
+        return True
+
+    def _generate_poly(self, pool_datas):
+        "create a polynomial fit curve of order self.order; should work for multidimensional arrays by flattening to columns"
+        base = pool_datas[-2].when        
+        xs = np.array([(x.when - base).total_seconds() for x in pool_datas])
+        # stack data as horizontal columns corresponding to the time offset columns
+        ys = np.hstack([q.data.ravel() for q in pool_datas])
+        LOG.debug(repr(xs))
+        LOG.debug(repr(ys))
+        coeffs = np.polyfit(xs, ys, self._order)
+        return pool_poly(pool_datas[-2].when, pool_datas[-1].when, coeffs)
+
+    def _apply_poly(self, poly, when):
+        s = (when - poly.start).total_seconds()
+        col = np.polyval(poly.coeffs, s)
+        LOG.debug('s=%f' % s)
+        # reshape to match original data
+        return col.reshape(self._shape)
+
+    def _poly_for_time(self, when):
+        # find the offset within the pool
+        dex = bisect.bisect_left(self._times, when)
+        # get the time key for the poly we want to use, if it exists in the poly cache
+        # any given polynomial only interpolates for the last two time points used to generate it
+        key = self._pool[dex].when
+        # assert(len(self._pool) == len(self._times))
+        # assert(self._pool[dex].when == self._times[dex])
+        # assert(self._times[dex] <= when)
+        # assert(self._pool[dex+1].when > when)
+        # assert(self._times[dex+1] > when)
+        LOG.debug('found %s after %s @ %d' % (when, key, dex))
+        poly = self._polys.get(key, None)
+        if poly is None:  # grab the right block of pool data, e.g. 0:2 for order 1
+            poly = self._generate_poly(self._pool[dex - (self._order-1) : dex + 2])
+            self._polys[poly.start] = poly
+            # assert(a.when == poly.start)
+        return poly
+
+    def __call__(self, when):
+        if when not in self:
+            s,e = self.span
+            raise ValueError("cannot extrapolate to time %s, coverage is %s ~ %s" % (when, s,e))
+        poly = self._poly_for_time(when)
+        data = self._apply_poly(poly, when)
+        # FIXME self._drain(when)
+        return data
+
+    def __str__(self):
+        pool_entries = len(self._pool)
+        polynomials = len(self._polys)
+        start, end = self.span
+        return """<TimeSeriesPolyInterp with %(pool_entries)d pool entries, %(polynomials)d polynomials, 
+        time range %(start)s ~ %(end)s>
+        """ % locals()
+
+
+
+
+class testInterp(unittest.TestCase):
+    """
+    guh
+    wuh
+    fnub
+    """
+    tspi = 1
+
+    def setUp(self):
+        self.tspi = TimeSeriesPolyInterp()
+        self.start = when = datetime.utcnow()
+        delta = timedelta(seconds=10)
+        t = [when - delta, when]
+        y = [1,1]
+        for n in range(8):
+            y.append(y[-2] + y[-1]) 
+            t.append(t[-1] + delta)  # this is okay because it's all integer arithmetic
+            print t[-1], y[-1]
+        print self.tspi
+        print ', '.join(str(x) for x in t)
+        print y
+        self.tspi += list((a,np.array(b, dtype=np.float32)) for (a,b) in zip(t,y))
+        self.reference = dict(zip(t,y))
+        print str(self.reference)
+        self.end = t[-1]
+
+    def testBasics(self):
+        assert(self.tspi is not None)
+        when = self.start
+        while when < self.end:
+            q = self.tspi(when)
+            print when, q, self.reference[when]
+            self.assertEqual(np.round(q), self.reference[when])
+            # self.assertTrue(np.abs(q - self.reference[when]) < 0.0005)
+            when += timedelta(seconds=20)
+
+
+# def suite():
+#     suite = unittest.TestSuite()
+#     suite.addTest(unittest.makeSuite(testInterp))
+#     return suite
+
+
+
+
+
+def main():
+    import optparse
+    usage = """
+%prog [options] ...
+
+
+"""
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-t', '--test', dest="self_test",
+                    action="store_true", default=False, help="run self-tests")
+    parser.add_option('-v', '--verbose', dest='verbosity', action="count", default=3,
+                    help='each occurrence increases verbosity 1 level through ERROR-WARNING-INFO-DEBUG')
+    # parser.add_option('-o', '--output', dest='output',
+    #                 help='location to store output')
+    # parser.add_option('-I', '--include-path', dest="includes",
+    #                 action="append", help="include path")                            
+    (options, args) = parser.parse_args()
+
+    # make options a globally accessible structure, e.g. OPTS.
+    global OPTS
+    OPTS = options
+
+    if options.self_test:
+        unittest.main()
+        return 0
+
+    levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
+    logging.basicConfig(level = levels[min(3,options.verbosity)])
+
+    if not args:
+        unittest.main()
+        return 0
+        # parser.error( 'incorrect arguments, try -h or --help.' )
+        # return 1
+
+    # FIXME main logic
+      
+    return 0
+
+
+
+
+if __name__=='__main__':
+    sys.exit(main())
+
+
